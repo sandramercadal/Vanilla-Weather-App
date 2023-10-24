87604ba32b5f4c64facc214bb872e89506ee7846
sandramerc <144231118+sandramercadal@users.noreply.github.com>
2023-10-24 18:18:51 +0100

    Removed duplicate icon (H5) and moved icon to correct location

--- script.js
+++ script.js
@@ -33,11 +33,11 @@ function getForecast(coordinates) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
-
+//checked
 function displayForecast(response) {
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
-  let forecast = response.data.daily;
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
     if (index < 6) {
@@ -46,9 +46,10 @@ function displayForecast(response) {
         `
       <div class="col-2">
      <div class="weather-forecast-date">${formatDay(forecastDay.time)}</div>
-  
-     <img
-          src="${forecastDay.condition.icon_url}"
+      <img
+      src="http://openweathermap.org/img/wn/${
+        forecastDay.weather[0].icon
+      }@2x.png"
           alt=""
           width="42"
         />
